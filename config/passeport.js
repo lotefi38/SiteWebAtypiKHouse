a41275b0131417160d3121d6e2419a453f774986
lotefi38 <lotefimohdeb@outlook.fr>
2024-07-08 18:10:10 +0200

    sixth commit

---
+++ config/passeport.js
@@ -0,0 +1,36 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const db = require('../models');
+
+function initialize(passport) {
+  const authenticateUser = async (username, password, done) => {
+    try {
+      const user = await db.User.findOne({ where: { username: username } });
+      if (!user) {
+        return done(null, false, { message: 'No user with that username' });
+      }
+
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (isValidPassword) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Password incorrect' });
+      }
+    } catch (err) {
+      return done(err);
+    }
+  };
+
+  passport.use(new LocalStrategy({ usernameField: 'username' }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await db.User.findByPk(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+}
+
+module.exports = initialize;
